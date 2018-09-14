ae63738f4cb08d18236d260e97278d1475c1ceeb
kszheng <kszheng@mail.ustc.edu .cn>
2018-09-14 16:06:33 +0800

    fix some bugs

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,16 +1,37 @@
 alias map_saver="rosrun map_server map_saver"
 
+source ~/catkin_cartographer/install_isolated/setup.bash
+
+prefix="roslaunch we_mapping"
+
 function online_mapping() {
-    roslaunch we_mapping demo_lidar_2d.launch bag_filename:=$1
+    $prefix demo_lidar_2d.launch bag_filename:=$1
 }
 
 function offline_mapping() {
-    roslaunch we_mapping demo_lidar_2d_offline.launch bag_filenames:=$1
+    $prefix demo_lidar_2d_offline.launch bag_filenames:=$1
 }
 
 function localization() {
-
+    $prefix demo_lidar_2d_pure_localization_bag.launch load_state_filename:=$1.pbstream bag_filename:=$1
 }
+
 function pbstream_to_ros_map() {
-    rosrun cartographer_ros cartographer_pbstream_to_ros_map $1
+    rosrun cartographer_ros cartographer_pbstream_to_ros_map -pbstream_filename $1
+}
+
+function visualize_pbstream() {
+    $prefix visualize_pbstream.launch pbstream_file:=$1
+}
+
+function localization_start_trajectory() {
+    $prefix lidar_2d_localization_start_trajectory.launch load_state_filename:=$1.pbstream bag_filename:=$1
+}
+
+function start_trajectory() {
+    if [ $# -eq 0 ]; then
+        $prefix start_trajectory.launch
+    elif [$# -eq 1]; then
+        $prefix start_trajectory.launch $1
+    fi
 }
